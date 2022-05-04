fbdae20cdcd78aecda950aa73e997f319271a19a
iankuys <iankuyisien@gmail.com>
2022-05-04 03:35:08 -0700

    Testing Fetch API on JS

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    print("hi")
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.form['appt']
+    return (data)
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    print("hello")
+
+    return ("hi")
 
 if __name__ == "__main__":
     app.run()
