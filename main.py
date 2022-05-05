1f07d34a13b841e76567b8559a724f8c9dbf460c
brandonkeung <blkeung@gmail.com>
2022-05-04 18:49:46 -0700

    Figured out the camera

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
-
+import cv2 as cv
+from pylab import *
+import mediapipe as mp
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -14,8 +16,21 @@ def login():
 
 @app.route('/open', methods=['GET', 'POST'])
 def home():
-    print("hello")
+    print("hello from open")
     return ("hi")
+
+@app.route('/capture', methods=['GET', 'POST'])
+def capture():     
+    cam_port = 0
+    cam = cv.VideoCapture(cam_port)
+    result, image = cam.read()
+    if result:
+        cv.imwrite("filename.png", image)
+    else:
+        print("No image detected. Please! try again")
+
+    return ("hi")
+    # figure out how to send image to another server (post request)
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
