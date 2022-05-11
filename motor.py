37b0b6ee16adbd421168f459cbc75a6b03ca0367
emily-truong <81642289+emily-truong@users.noreply.github.com>
2022-05-11 02:21:18 -0700

    Outline (comments only)

--- motor.py
+++ motor.py
@@ -7,4 +7,24 @@ import RPi.GPIO as GPIO
 # https://github.com/vishytheswishy/junk-transporter-backend/blob/main/motor.py
 
 
-# for more info:
+import RPi.GPIO as GPIO    
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(en,GPIO.OUT)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+# p=GPIO.PWM(en,1000)
+p.start(25)
+
+while True: 
+
+# Opening curtain 
+  # Gesture control --> motor starts
+  # Timer --> motor starts
+
+# When the curtain is extending and limit switch is hit --> motor stops
+# When the curtain is retracting and limit switch is hit --> motor stops
+
+# If disconnected break
