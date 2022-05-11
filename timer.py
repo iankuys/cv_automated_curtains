b7c8809b193126b14cc49ff9a4f126a4e3b6271f
rayhuaruijia <56495557+rayhuaruijia@users.noreply.github.com>
2022-05-11 00:17:12 -0700

    change timer.py

--- timer.py
+++ timer.py
@@ -11,12 +11,11 @@ import time
 def getTime():
     return time.asctime()[11:19]
 
-def getPresetTime():
-    # function to receive time given from the web app
-    return
-
 def checkTimeMatches():
     if getTime() == getPresetTime():
         return True
     else:
         return False
+
+if __name__ == "__main__":
+    print(getTime())
